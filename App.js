e41c1e9da4c38142c96048beb3707d4cc350c815
jueun-park98 <jooeun06161@gmail.com>
2024-04-15 18:43:12 +0900

    chore: express 설정 추가

---
+++ App.js
@@ -0,0 +1,17 @@
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "build")));
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
