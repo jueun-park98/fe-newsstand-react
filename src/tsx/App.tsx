c3ace777f29fe2dc0a6ed1155793aba2a32c6c04
jueun-park98 <jooeun06161@gmail.com>
2024-04-15 17:31:05 +0900

    feat: 헤더 추가

--- src/tsx/App.tsx
+++ src/tsx/App.tsx
@@ -1,23 +1,16 @@
 import React from "react";
-import logo from "../img/logo.svg";
 import "../css/App.css";
+import Header from "./Header";
+import RollingContainer from "./RollingContainer";
+import PressContainer from "./PressContainer";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+      <Header></Header>
+      <RollingContainer></RollingContainer>
+      <PressContainer></PressContainer>
     </div>
-    // <Header></Header>
-    // <RollingContainer></RollingContainer>
-    // <PressContainer></PressContainer>
   );
 }
 
