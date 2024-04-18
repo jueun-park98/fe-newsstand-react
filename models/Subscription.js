7768f8abc67c1508cc5010b3f9b40f62fde60a79
jueun-park98 <jooeun06161@gmail.com>
2024-04-18 15:33:54 +0900

    feat: mongoose 모델, 초기화 기능 추가

---
+++ models/Subscription.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const subscriptionSchema = new mongoose.Schema({
+  id: String,
+  pressName: String,
+  logoImageSrc: String,
+  editedTime: String,
+  category: String,
+  headline: {
+    thumbnailSrc: String,
+    title: String,
+    href: String,
+  },
+  sideNews: [{ title: String, href: String }],
+});
+
+const Subscription = mongoose.model("Subscription", subscriptionSchema)
+
+export default Subscription;
--- routes/news.js
+++ routes/news.js
@@ -1,30 +1,15 @@
 import express from "express";
-import fs from "fs";
-import path from "path";
-import { __dirname } from "../App.js";
+import News from "../models/News.js";
 
 const newsRouter = express.Router();
 
-newsRouter.get("/", (req, res) => {
-  const newsPath = path.join(__dirname, "data/news.json");
-
-  fs.readFile(newsPath, "utf-8", (err, data) => {
-    if (err) {
-      console.error("Error reading news: ", err);
-      res.status(500).send("Server Error");
-      return;
-    }
-
-    try {
-      const news = JSON.parse(data).news;
-
-      res.set("Cache-Control", "public, max-age=300");
-      res.json(news);
-    } catch (parseError) {
-      console.error("Error parsing news: ", parseError);
-      res.status(500).send("Server Error");
-    }
-  });
+newsRouter.get("/", async (req, res) => {
+  try {
+    const news = await News.find();
+    res.json(news);
+  } catch (error) {
+    res.status(500).send("Server Error");
+  }
 });
 
 export default newsRouter;
