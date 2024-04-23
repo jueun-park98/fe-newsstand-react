2eb53d01ca486e7a6c2ef2978285aa4422fbd607
jueun-park98 <jooeun06161@gmail.com>
2024-04-23 12:08:19 +0900

    feat: 구독 리스트 fetch 요청 기능 추가

--- routes/subscription.js
+++ routes/subscription.js
@@ -8,7 +8,31 @@ subscriptionRouter.get("/", async (req, res) => {
     const subscriptions = await Subscription.find();
     res.json(subscriptions);
   } catch (error) {
-    res.status(500).send("Server Error")
+    res.status(500).send("Server Error");
+  }
+});
+
+subscriptionRouter.post("/", async (req, res) => {
+  try {
+    const { news } = req.body;
+    const newSubscription = new Subscription(news);
+
+    await newSubscription.save();
+    res.status(201).json(newSubscription);
+  } catch (error) {
+    res.status(500).send("Server Error");
+  }
+});
+
+subscriptionRouter.delete("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const result = await Subscription.findByIdAndDelete(id);
+
+    if (!result) res.status(404).send("Subscription not found");
+    res.status(200).json({ message: "Subscription deleted successfully" });
+  } catch (error) {
+    res.status(500).send("Server Error");
   }
 });
 
--- src/tsx/api/NewsAPI.tsx
+++ src/tsx/api/NewsAPI.tsx
@@ -1,3 +1,5 @@
+import { News } from "../components/constants";
+
 const SERVER = process.env.REACT_APP_SERVER;
 
 export const fetchNews: () => Promise<object[]> = async () => {
@@ -12,4 +14,45 @@ export const fetchNews: () => Promise<object[]> = async () => {
   const news = response.json();
 
   return news;
+};
+
+export const fetchSubscription: () => Promise<object[]> = async () => {
+  const request = {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+  const response = await fetch(`${SERVER}/subscription`, request);
+  if (!response.ok) throw new Error("Network Error");
+  const subscription = response.json();
+
+  return subscription;
+};
+
+export const postSubscription: (news: News) => Promise<Response> = async (news) => {
+  const request = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(news),
+  };
+  const response = await fetch(`${SERVER}/subscription`, request);
+  if (!response.ok) throw new Error("Network Error");
+
+  return response;
+};
+
+export const deleteSubscription: (id: string) => Promise<Response> = async (id) => {
+  const request = {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+  const response = await fetch(`${SERVER}/subscription/${id}`, request);
+  if (!response.ok) throw new Error("Network Error");
+
+  return response;
 };
